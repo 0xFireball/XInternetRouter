5c2a5b4d079f88fb8547dc03c2142179cdb92c58
0xFireball <0xFireball@outlook.com>
2016-05-19 08:55:29 -0700

    update assemblyinfo, add ignore

--- ZInternetRouter/ZInternetRouter.Server.Core/Properties/AssemblyInfo.cs
+++ ZInternetRouter/ZInternetRouter.Server.Core/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ZInternetRouter.Server.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ZInternetRouter Core")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ZetaPhase Technologies, Inc.")]
 [assembly: AssemblyProduct("ZInternetRouter.Server.Core")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2016 ZetaPhase Technologies")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
